4cc4f4814a68eb55409fb8057adf516b6b31551c
Nishan Kantan <nishan.kantan@gmail.com>
2022-04-26 10:46:43 +0800

    add url for training view

--- bank_ocr/urls.py
+++ bank_ocr/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     # path('post', CreatePostView.as_view(), name='add_post'),
     path('post', views.upload_pdf, name='add_post'),
+    path('training', TrainingView.as_view(), name='training'),
     path('result', ResultView.as_view(), name='result'),
 ]
