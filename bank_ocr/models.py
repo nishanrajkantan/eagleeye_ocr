2dc13a5ffbf84f12a95d125e7d65cb14e0d69a9e
Nishan Kantan <nishan.kantan@gmail.com>
2022-04-26 10:46:32 +0800

    add training pdf files field

--- bank_ocr/models.py
+++ bank_ocr/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Post (models.Model):
     title = models.CharField(max_length=200)
     image = models.FileField(upload_to='images/')
+    trainingpdf = models.FileField(upload_to='trainingpdf/', blank = True, null = True)
     processed_image = models.FileField(upload_to='processing_images/', blank = True, null = True)
     description = models.CharField(max_length=400, blank = True, null = True)
 
